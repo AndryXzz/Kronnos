a35e2fb128799e34bf4394ee640fa04c37a9a380
fsanchez96nov <fabiandsanchezb@hotmail.com>
2020-12-07 16:04:41 -0500

    trabajo final

--- credihogar_tienda.php
+++ credihogar_tienda.php
@@ -1,245 +1,241 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CrediHogar</title>
+    <title>Ecommerce/credihogar</title>
     <link rel="stylesheet" href="css/estilostienda.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
-
+<link rel="shortcut icon" href="img/img_tienda/credihogar_marcas.jpg">
 <body>
-
-    <div class="headeer">
-        <div class="container">
-            <div class="navbar">
-                <div class="logo">
-                    <img src="./img/img_tienda/kronnos_logo.png" alt="">
-                </div>
-                <nav>
-                    <ul>
-                        <li><a href="">home</a></li>
-                        <li><a href="">productos</a></li>
-                        <li><a href="">contacto</a></li>
-                        <li><a href="">about</a></li>
-                    </ul>
-                </nav>
-                <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+<div class="headeer">
+    <div class="container">
+        <div class="navbar">
+            <div class="logo">
+                <img src="img/img_tienda/kronnos_logo.png" alt="">
             </div>
-            <div class="row">
-                <div class="col-2">
-                    <h1>CrediHogar</h1>
-                    <p>Lenceria para el hogar y todo lo que necesitas en un click</p>
-                    <a href="" class="btn"> Click Aqui </a>
-                </div>
-                <div class="col-2">
-                    <img src="./img/img_tienda/fondo_intro_web_kronnos.jpg" width="600px" alt="">
-                </div>
-            </div>
-
-
+            <nav>
+                <ul>
+                    <li><a href="credihogar_tienda.php">home</a></li>
+                    <li><a href="vistas/tienda/lenceria_credihogar_1click.html">productos</a></li>
+                  <!--  <li><a href="">contacto</a></li>
+                    <li><a href="">about</a></li> --->
+                </ul>
+            </nav>
+           <a href="vistas/tienda/formu_regis.html"><i class="fa fa-shopping-cart" aria-hidden="true"></i></a>
         </div>
-    </div>
-    <!-------- featured categories ------->
-    <div class="categories">
-        <div class="small-container">
-            <div class="row">
-                <div class="col-3">
-                    <img src="./img/img_tienda/lenceria_intro3.jpg" alt="">
-                </div>
-                <div class="col-3">
-                    <img src="./img/img_tienda/cama_intro1.jpg" alt="">
-                </div>
-                <div class="col-3">
-                    <img src="./img/img_tienda/cama_intro2.jpg" alt="">
-                </div>
-            </div>
-
-        </div>
-    </div>
-    <!-------- featured products ------->
-    <div class="small-container">
-        <h2 class="tittle">Productos Destacados</h2>
         <div class="row">
+            <div class="col-2">
+                <h1>CrediHogar</h1>
+                <p>Lenceria para el hogar y todo lo que necesitas en un click</p>
+                <a href="vistas/tienda/lenceria_credihogar_1click.html"class="btn"> Click Aqui </a>
+            </div>
+                <div class="col-2"> 
+                    <img src="img/img_tienda/fondo_intro_web_kronnos.jpg" width="600px" alt="">
+            </div>
+        </div>
+
+
+    </div>
+</div> 
+<!-------- featured categories ------->
+    <div class="categories">
+    <div class="small-container">
+      <div class="row">
+          <div class="col-3">
+              <img src="img/img_tienda/lenceria_intro3.jpg" alt="">
+          </div>
+          <div class="col-3">
+              <img src="img/img_tienda/cama_intro1.jpg" alt="">
+          </div>
+          <div class="col-3">
+            <img src="img/img_tienda/cama_intro2.jpg" alt="">
+        </div>
+    </div>   
+          
+      </div>
+    </div>
+<!-------- featured products ------->
+<div class="small-container">
+    <h2 class="tittle">Productos Destacados</h2>
+    <div class="row">
             <div class="col-4">
-                <img src="./img/img_tienda/felpa-crema_12.jpg">
+                <img src="img/img_tienda/felpa-crema_12.jpg">
                 <h4>Felpa Color Crema</h4>
                 <div class="ratting">
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star-half-o"></i>
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star-half-o" ></i>
                     <i class="fa fa-star-o"></i>
                     <p>$50.000</p>
                 </div>
             </div>
             <div class="col-4">
-                <img src="./img/img_tienda/duvet_sabanaajustable.jpg">
+                <img src="img/img_tienda/duvet_sabanaajustable.jpg">
                 <h4>Sabana Ajustable</h4>
                 <div class="ratting">
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star" ></i>
                     <i class="fa fa-star-o"></i>
                     <p>$85.000</p>
                 </div>
+            </div>         
+     </div>
+     <h2 class="tittle">ultima coleccion</h2>
+     <div class="row">
+        <div class="col-4">
+            <img src="img/img_tienda/juego-forro-duvet-sabana-hotelero.jpg">
+            <h4>Felpa Color Crema</h4>
+            <div class="ratting">
+                <i class="fa fa-star" ></i>
+                <i class="fa fa-star" ></i>
+                <i class="fa fa-star" ></i>
+                <i class="fa fa-star" ></i>
+                <i class="fa fa-star-o"></i>
+                <p>$150.000</p>
             </div>
         </div>
-        <h2 class="tittle">ultima coleccion</h2>
+        <div class="col-4">
+            <img src="img/img_tienda/forro_duvet_negro.jpg">
+            <h4>Sabana Ajustable</h4>
+            <div class="ratting">
+                <i class="fa fa-star" ></i>
+                <i class="fa fa-star" ></i>
+                <i class="fa fa-star" ></i>
+                <i class="fa fa-star" ></i>
+                <i class="fa fa-star-o"></i>
+                <p>$205.000</p>
+            </div>
+        </div>       
         <div class="row">
             <div class="col-4">
-                <img src="./img/img_tienda/juego-forro-duvet-sabana-hotelero.jpg">
+                <img src="img/img_tienda/cama_intro1.jpg">
                 <h4>Felpa Color Crema</h4>
                 <div class="ratting">
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star" ></i>
                     <i class="fa fa-star-o"></i>
                     <p>$150.000</p>
                 </div>
             </div>
             <div class="col-4">
-                <img src="./img/img_tienda/forro_duvet_negro.jpg">
+                <img src="img/img_tienda/juego_sabanas_dobleplanchado.jpg">
                 <h4>Sabana Ajustable</h4>
                 <div class="ratting">
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star" ></i>
                     <i class="fa fa-star-o"></i>
                     <p>$205.000</p>
                 </div>
-            </div>
+            </div> 
+                   
+</div>
+<!--------offer-------->
+    <div class="offer">
+        <div class="small-container">
             <div class="row">
-                <div class="col-4">
-                    <img src="./img/img_tienda/cama_intro1.jpg">
-                    <h4>Felpa Color Crema</h4>
-                    <div class="ratting">
-                        <i class="fa fa-star"></i>
-                        <i class="fa fa-star"></i>
-                        <i class="fa fa-star"></i>
-                        <i class="fa fa-star"></i>
-                        <i class="fa fa-star-o"></i>
-                        <p>$150.000</p>
-                    </div>
+                <div class="col-2">
+                    <img src="img/img_tienda/doble_planchado_click_comprar.png" class="offer-img">
                 </div>
-                <div class="col-4">
-                    <img src="./img/img_tienda/juego_sabanas_dobleplanchado.jpg">
-                    <h4>Sabana Ajustable</h4>
-                    <div class="ratting">
-                        <i class="fa fa-star"></i>
-                        <i class="fa fa-star"></i>
-                        <i class="fa fa-star"></i>
-                        <i class="fa fa-star"></i>
-                        <i class="fa fa-star-o"></i>
-                        <p>$205.000</p>
-                    </div>
-                </div>
-
-            </div>
-            <!--------offer-------->
-            <div class="offer">
-                <div class="small-container">
-                    <div class="row">
-                        <div class="col-2">
-                            <img src="./img/img_tienda/doble_planchado_offer.webp" class="offer-img">
-                        </div>
-                        <div class="col-2">
-                            <p>Exclusivos/Credihogar</p>
-                            <h1>Sabana Doble Planchado</h1>
-                            <small>Juego de sabanas para Cama Doble en material 100% Poliester de textura suave y
-                                fresca, generando máximo confort, garantizadas de muy buena calidad no pierde
-                                color.</small>
-                            <a href="" class="btn">Comprar Ahora &#8594;</a>
-                        </div>
-                    </div>
+                <div class="col-2">
+                    <p>Exclusivos/Credihogar</p>
+                    <h1>Sabana Doble Planchado</h1>
+                    <small>Juego de sabanas para Cama Doble en material 100% Poliester de textura suave y fresca, generando máximo confort, garantizadas de muy buena calidad no pierde color.</small>
+                    <a href="vistas/tienda/doble_planchado_offer_click_comprar.html" class="btn">Comprar Ahora &#8594;</a>
                 </div>
             </div>
+        </div>
+    </div>
 
-            <!----------Testimonios------>
-            <div class="testimonial">
-                <div class="small-container">
-                    <div class="row">
-                        <div class="col-3">
-                            <i class="fa fa-quote-left"></i>
-                            <p>Especificaciones <br>
+    <!----------Testimonios------>
+    <div class="testimonial">
+        <div class="small-container">
+            <div class="row">
+                <div class="col-3">
+                    <i class="fa fa-quote-left"></i>
+                    <p>Especificaciones <br>
 
-                                *Tipo: Juego de Sabanas 4 Piezas. <br>
-                                *Incluye: Sabana, Sobresabana y 2 Fundas. <br>
-                                *Dimensiones Sabana: 203 x 235 Cm Aprox <br>
-                                *Dimensiones Forro: 137 x 190 + 21 Cm Aprox <br>
-                                *Dimensiones Funda: 50 x 76 Cm Aprox <br>
-                                *Número de hilos: 150 <br>
-                                *Planchado Permanente. <br></p>
-                            <div class="ratting">
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <p>$205.000</p>
-                            </div>
-                            <img src="./img/img_tienda/kronnos_logo.png">
+                        *Tipo: Juego de Sabanas 4 Piezas. <br>
+                        *Incluye: Sabana, Sobresabana y 2 Fundas. <br>
+                        *Dimensiones Sabana: 203 x 235 Cm Aprox <br>
+                        *Dimensiones Forro: 137 x 190 + 21 Cm Aprox <br>
+                        *Dimensiones Funda: 50 x 76 Cm Aprox <br>
+                        *Número de hilos: 150 <br>
+                        *Planchado Permanente. <br></p>
+                        <div class="ratting">
+                            <i class="fa fa-star" ></i>
+                            <i class="fa fa-star" ></i>
+                            <i class="fa fa-star" ></i>
+                            <i class="fa fa-star" ></i>
+                            <i class="fa fa-star"></i>
+                            <p>$205.000</p>
                         </div>
-                        <div class="col-3">
-                            <i class="fa fa-quote-left"></i>
-                            <p>Juego de sábanas para cama de 1.40 x 1.90 en Algodón de 180 hilos
-
-                                Son sábanas frescas.
-                                No le salen motas
-                                No se encogen
-                                No se arrugan <br> Incluye: <br>
-
-                                1 sobre sábana, para colchón de máximo 30 cm de altura.
-                                1 sábana ajustable
-                                2 fundas para almohadas</p>
-                            <div class="ratting">
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <p>$205.000</p>
-                            </div>
-                            <br>
-                            <img src="./img/img_tienda/kronnos_logo.png">
-
-                        </div>
-                    </div>
+                        <img src="img/kronnos_logo.png" >
                 </div>
+                <div class="col-3">
+                    <i class="fa fa-quote-left"></i>
+                    <p>Juego de sábanas para cama de 1.40 x 1.90 en Algodón de 180 hilos
 
-                <!-------Marcas--------->
-                <div class="brands">
-                    <div class="small-container">
-                        <div class="row">
-                            <div class="col-5">
-                                <img src="./img/img_tienda/credihogar_marcas.jpg">
-                            </div>
-                            <div class="col-5">
-                                <img src="./img/img_tienda/yuzmar_marcas.jpg">
-                            </div>
-                            <div class="col-5">
-                                <img src="./img/img_tienda/hogar_decor_marcas.png">
-                            </div>
+                        Son sábanas frescas.
+                        No le salen motas
+                        No se encogen
+                        No se arrugan <br> Incluye: <br>
+                       
+                        1 sobre sábana, para colchón de máximo 30 cm de altura.
+                        1 sábana ajustable
+                        2 fundas para almohadas</p>
+                        <div class="ratting">
+                            <i class="fa fa-star" ></i>
+                            <i class="fa fa-star" ></i>
+                            <i class="fa fa-star" ></i>
+                            <i class="fa fa-star" ></i>
+                            <i class="fa fa-star"></i>
+                            <p>$205.000</p>
                         </div>
-                    </div>
-                </div>
+                        <br>
+                        <img src="img/kronnos_logo.png" >
 
-                <!----------------footer----------------->
-                <div class="footer">
-                    <div class="container">
-                        <div class="row">
-                            <div class="footer-col-1">
-                                <h3></h3>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            </div>
+        </div>
+    </div>
 
+ <!-------Marcas--------->   
+ <div class="brands">
+     <div class="small-container">
+         <div class="row">
+             <div class="col-5">
+                 <img src="img/img_tienda/credihogar_marcas.jpg" >
+             </div>
+             <div class="col-5">
+                <img src="img/img_tienda/yuzmar_marcas.jpg" >
+            </div>
+            <div class="col-5">
+                <img src="img/img_tienda/hogar_decor_marcas.png" >
+            </div>
+         </div>
+     </div>
+ </div>
+
+<!----------------footer----------------->
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="footer-col-1">
+                <h3></h3>
+            </div>
+        </div>
+    </div>
+</div>
 
 </body>
-
 </html>
